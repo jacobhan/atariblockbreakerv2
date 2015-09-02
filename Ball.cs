8f5561f830bef1f0d145d2ea98920a4afea24885
jacobhan <jungwoohoo@gmail.com>
2015-09-02 01:53:22 -0700

    Update Ball.cs

--- Ball.cs
+++ Ball.cs
@@ -6,3 +6,11 @@ public class Ball : MonoBehaviour {
 	private Paddle paddle;
 	private bool hasStarted = false;
 	private Vector3 paddleToBallVector;
+
+// Use this for initialization
+	void Start () {
+		paddle = GameObject.FindObjectOfType<Paddle>();
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+	}
+
+
