5039dccc97d79bf4b61e429206d1d5e8a2ac830b
jacobhan <jungwoohoo@gmail.com>
2015-09-03 00:49:09 -0700

    Update Ball.cs

--- Ball.cs
+++ Ball.cs
@@ -7,10 +7,12 @@ public class Ball : MonoBehaviour {
 	private bool hasStarted = false;
 	private Vector3 paddleToBallVector;
 
-// Use this for initialization
+	// Use this for initialization
 	void Start () {
 		paddle = GameObject.FindObjectOfType<Paddle>();
 		paddleToBallVector = this.transform.position - paddle.transform.position;
 	}
+		
+	// Update is called once per frame
 
 
