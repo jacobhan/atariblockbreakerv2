c6e6c3d78b8b14e4dc7bb173ca1f7ff2931d8132
jacobhan <jungwoohoo@gmail.com>
2015-09-07 01:06:36 -0700

    Update Ball.cs

--- Ball.cs
+++ Ball.cs
@@ -28,6 +28,8 @@ public class Ball : MonoBehaviour {
 	}
 	
 	void OnCollisionEnter2D (Collision2D collision) {
+		
+		Vector2 tweak = new Vector2 (Random.Range(0f, 0.2f), Random.Range(0f, 0.2f));
 
 
 
