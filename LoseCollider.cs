edeb24a4d2ee0030f8d740e03e2eaf08e36780cf
jacobhan <jungwoohoo@gmail.com>
2015-09-13 00:05:44 -0700

    Update LoseCollider.cs

--- LoseCollider.cs
+++ LoseCollider.cs
@@ -5,11 +5,14 @@ using System.Collections;
 public class LoseCollider : MonoBehaviour {
     
 	private LevelManager levelManager;
+	
 	void OnTriggerEnter2D (Collider2D trigger) {
 		levelManager = GameObject.FindObjectOfType<LevelManager>();
 		levelManager.LoadLevel("Loose Screen");
 	}
 	
-	
+	void OnCollisionEnter2D (Collision2D collision) {
+		
+	}
 
 }
