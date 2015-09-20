52a9c1509bea4361ff82c3f5aed563e583fe3d6f
jacobhan <jungwoohoo@gmail.com>
2015-09-19 22:38:40 -0700

    Update LevelManager.cs

--- LevelManager.cs
+++ LevelManager.cs
@@ -2,6 +2,6 @@ using UnityEngine;
 using System.Collections;
 
 public class LevelManager : MonoBehaviour {
-  public void LoadLevel(string name){
+  public void LoadLevel(string name) {
 		Debug.Log ("New Level load: " + name);
   
