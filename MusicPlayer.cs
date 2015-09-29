4a5fc1e671afacbc317da3deffc9d81eb13a0435
jacobhan <jungwoohoo@gmail.com>
2015-09-28 17:28:44 -0700

    Update MusicPlayer.cs

--- MusicPlayer.cs
+++ MusicPlayer.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 using System.Collections;
 
 public class MusicPlayer : MonoBehaviour {
-  static MusicPlayer instance = null;
+	static MusicPlayer instance = null;
 
-  // Use this for initialization
+  	// Use this for initialization
 	void Start () {
+		
+		
+		
