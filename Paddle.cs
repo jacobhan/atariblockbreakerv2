7a913197ffd73e696476e8724bc0f7c34d7902d2
jacobhan <jungwoohoo@gmail.com>
2015-10-13 19:48:24 -0700

    Update Paddle.cs

--- Paddle.cs
+++ Paddle.cs
@@ -24,3 +24,4 @@ public class Paddle : MonoBehaviour {
 	void AutoPlay() {
 		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);
 		Vector3 ballPos = ball.transform.position;
+		paddlePos.x = Mathf.Clamp(ballPos.x, minX, maxX);
