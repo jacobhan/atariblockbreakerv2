6f98ecce9e0fb3255c984d7765bea9ab925b073d
jacobhan <jungwoohoo@gmail.com>
2015-10-15 00:01:49 -0700

    Update Paddle.cs

--- Paddle.cs
+++ Paddle.cs
@@ -31,4 +31,4 @@ public class Paddle : MonoBehaviour {
 	void MoveWithMouse () {
 		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);
 		float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
-		
+		paddlePos.x = Mathf.Clamp(mousePosInBlocks, minX, maxX);
